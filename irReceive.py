02a17a4e0aed3bde79b320f2e8bec78eafcecc94
Brian Schwind <brianmschwind@gmail.com>
2016-01-26 00:17:03 +0900

    Create irReceive.py

---
+++ irReceive.py
@@ -0,0 +1,56 @@
+import RPi.GPIO as GPIO
+import math
+import os
+from datetime import datetime
+from time import sleep
+
+INPUT_WIRE = 26
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(INPUT_WIRE, GPIO.IN)
+
+while True:
+	value = 1
+	while value:
+		value = GPIO.input(INPUT_WIRE)
+
+	# Grab the start time of the command
+	startTime = datetime.now()
+
+	# Used to buffer the command pulses
+	command = []
+
+	# The end of the "command" happens when we read more than
+	# a certain number of 1s (1 is off for my IR receiver)
+	numOnes = 0
+
+	# Used to keep track of transitions from 1 to 0
+	previousVal = 0
+
+	while True:
+
+		if value != previousVal:
+			now = datetime.now()
+			pulseLength = now - startTime
+			startTime = now
+
+			command.append((previousVal, pulseLength.microseconds))
+
+		if value:
+			numOnes = numOnes + 1
+		else:
+			numOnes = 0
+
+		# 10000 is arbitrary, adjust as necessary
+		if numOnes > 10000:
+			break
+
+		previousVal = value
+		value = GPIO.input(INPUT_WIRE)
+	
+	print "----------Start----------"
+	for (val, pulse) in command:
+		print val, pulse
+	print "-----------End-----------\n"
+
+	print "Size of array is " + str(len(command))
